08e9e6f2fa4e67e66122ccb28829782dd7917657
Helsa Nesta <70515589+helsanesta@users.noreply.github.com>
2023-05-16 23:18:53 +0700

    add base component todoapp

---
+++ src/components/TodoApp.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class TodoApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <div className="TodoApp">
+                <h1>TodoApp</h1>
+            </div>
+        );
+    }
+}
+
+export default TodoApp;
